bcd79b34854cab6b1590ed48042b72c89e1d2216
HamzaAlsoodani <alsoodanihamza6@gmail.com>
2024-04-25 11:13:13 +0100

    Updated the horse class with encapsulation and getters/setters.

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -2,59 +2,68 @@
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name)
+ * @author Hamza Alsoodani
  * @version (a version number or a date)
  */
 public class Horse {
-    // Fields of class Horse
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean fallen;
+    private double confidence;
 
-    // Constructor of class Horse
-    /**
-     * Constructor for objects of class Horse
-     */
+    // Constructor
     public Horse(char horseSymbol, String horseName, double horseConfidence) {
-
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        setConfidence(horseConfidence);
+        this.distanceTravelled = 0;
+        this.fallen = false;
     }
 
-    // Other methods of class Horse
     public void fall() {
-
+        this.fallen = true;
     }
 
     public double getConfidence() {
-
+        return this.confidence;
     }
 
     public int getDistanceTravelled() {
-
+        return this.distanceTravelled;
     }
 
     public String getName() {
-
+        return this.name;
     }
 
     public char getSymbol() {
-
+        return this.symbol;
     }
 
     public void goBackToStart() {
-
+        this.distanceTravelled = 0;
+        this.fallen = false;
     }
 
     public boolean hasFallen() {
-
+        return this.fallen;
     }
 
     public void moveForward() {
-
+        if (!this.fallen) {
+            this.distanceTravelled += 1;
+        }
     }
 
     public void setConfidence(double newConfidence) {
-
+        if (newConfidence < 0 || newConfidence > 1) {
+            throw new IllegalArgumentException("Confidence must be between 0 and 1.");
+        }
+        this.confidence = newConfidence;
     }
 
     public void setSymbol(char newSymbol) {
-
+        this.symbol = newSymbol;
     }
-
 }
